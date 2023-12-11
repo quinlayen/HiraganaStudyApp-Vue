1f6594d4f575732bc42ec4cc62df5bae5e106167
Peter Faso <quinlayen@gmail.com>
2023-12-10 17:02:05 -1000

    scaffolded Vue applicaiton and created setup scripts

---
+++ setup/init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+################
+# Run this file first
+################
+
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed. Installing Python 3..."
+    # Install Python 3 (modify this line if you have a specific way to install Python)
+    brew install python3
+else
+    # Check if the installed Python version is at least 3.8
+    PYTHON_VERSION=$(python3 -V | cut -d ' ' -f 2)
+    REQUIRED_VERSION="3.8"
+    if [[ $(echo -e "$PYTHON_VERSION\n$REQUIRED_VERSION" | sort -V | head -n1) == $REQUIRED_VERSION ]]; then
+        echo "Python 3.8 or newer is already installed."
+    else
+        echo "Python 3 is installed but version is less than 3.8. Installing a newer version..."
+        # Install a newer version of Python 3 (modify this line if you have a specific way to install Python)
+        brew install python3
+    fi
+fi
+
+# Create and activate a virtual environment called sandbox
+echo "Creating and activating virtual environment 'sandbox'..."
+python3 -m venv sandbox
+source sandbox/bin/activate
+
+# Install dependencies from requirements.txt
+echo "Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+# Change permission of run.sh to 700
+echo "Changing permission of run.sh to 700..."
+chmod 700 run.sh
+
+# Create a directory called log if it doesn't exist
+echo "Creating a 'log' directory..."
+mkdir -p log
+
+echo "Script completed."
---
+++ setup/run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+##############################
+
+shopt -s xpg_echo
+
+##############################
+# SET DEFAULT VARIABLES
+HOST=$HOSTNAME
+SHORT_DATE=`date '+%Y-%m-%d'`
+TIME=`date '+%H%M'`
+SCRIPT_TYPE=`basename $0 | cut -d '.' -f1`
+filenametime1=$(date +"%m%d%Y%H%M%S")
+filenametime2=$(date +"%Y-%m-%d %H:%M:%S")
+
+##############################
+# SET VARIABLES
+export PYTHON_SCRIPT_NAME=$(cat config.toml | grep 'py_script' | awk -F"=" '{print $2}' | tr -d '"')
+export SCRIPTS_FOLDER=$(pwd)
+export LOGDIR=$SCRIPTS_FOLDER/log
+export SHELL_SCRIPT_NAME='run'
+export LOG_FILE=${LOGDIR}/${SHELL_SCRIPT_NAME}_${filenametime1}.log
+
+##############################
+# GO TO SCRIPT FOLDER AND RUN
+# cd ${SCRIPTS_FOLDER}
+
+##############################
+# SET LOG RULES
+exec > >(tee ${LOG_FILE}) 2>&1
+##############################
+# RUN SCRIPT
+source sandbox/bin/activate
+
+echo "Start Python script"
+python3 ${SCRIPTS_FOLDER/$PYTHON_SCRIPT_NAME}
+
+RC1=$?
+if [ ${RC1 -ne 0 } ]; then
+    echo "PYTHON RUNNING FAILED"
+    echo "[ERROR:] RETURN CODE: ${RC1}"
+    echo "[ERROR:] REFER TO THE LOG FOR THE REASON FOR THE FAILURE"
+    exit 1
+fi
+
+echo "PROGRAM SUCCEEDED"
+exit 0
+
+deactivate
+
+
